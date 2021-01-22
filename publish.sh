0d209a436cd2ad09e96ab9ba0cbd71fe21d243db
wangjj <wangjj@yunjiaclund.com>
2021-01-22 15:58:38 +0800

    [build] 1.0.13

--- publish.sh
+++ publish.sh
@@ -7,24 +7,30 @@ read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  # 处理 package.json
-  npm version $VERSION
-  echo '\033[36m版本号修改成功\033[0m'
+  # commit
+  git add -A
+  git commit -m "[build] $VERSION"
+  npm version $VERSION --message "[release] $VERSION"
+
+  # publish
+  git push origin master
+  git push origin refs/tags/v$VERSION
+  git rebase master
 
   #set npm config
   npm config set registry https://registry.npmjs.org/
 
   echo "Releasing wangeditor-vue $VERSION ..."
 
-  echo '\033[36m请进行登录相关操作：\033[0m'
+  echo '请进行登录相关操作：'
 
   npm login #登录
 
-  echo "-------\033[36mpublishing\033[0m-------"
+  echo "-------publishing-------"
 
   npm publish # 发布
 
-  echo "\033[36m 完成 \033[0m"
-  exit
+  echo " 完成 "
 
 fi
+exit
