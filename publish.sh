6abe9f18ba6f740d091513c8c14db4f2f1b631f2
wangjj <wangjj@yunjiaclund.com>
2021-01-22 15:41:42 +0800

    shell update

--- publish.sh
+++ publish.sh
@@ -14,18 +14,6 @@ then
   #set npm config
   npm config set registry https://registry.npmjs.org/
 
-  
-  # commit
-  git add -A
-  git commit -m "[build] $VERSION"
-
-  # publish
-  git push
-  git push origin refs/tags/v$VERSION
-  # git checkout dev
-  git rebase master
-  # git push dev
-
   echo "Releasing wangeditor-vue $VERSION ..."
 
   echo '\033[36m请进行登录相关操作：\033[0m'
